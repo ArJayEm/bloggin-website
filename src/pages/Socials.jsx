f2f191242cf69f5000fc8c89a26c8815beb6271f
Raff Julius Martinez <raffmartinez14@gmail.com>
2022-10-23 01:47:53 +0800

    reverse developer contact section and socials section

--- src/pages/Socials.jsx
+++ src/pages/Socials.jsx
@@ -10,8 +10,8 @@ export default function Socials() {
     <>
       <Navigation />
       <Container>
-        <SocialsSection />
         <DeveloperSection/>
+        <SocialsSection />
       </Container>
     </>
   );
